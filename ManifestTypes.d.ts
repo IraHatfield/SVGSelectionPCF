49f721991d112b92b4264aed8469c95aa1dedacc
IraHatfield <109093644+IraHatfield@users.noreply.github.com>
2024-01-19 11:17:48 -0600

    Add files via upload

---
+++ ManifestTypes.d.ts
@@ -0,0 +1,19 @@
+/*
+*This is auto generated from the ControlManifest.Input.xml file
+*/
+
+// Define IInputs and IOutputs Type. They should match with ControlManifest.
+export interface IInputs {
+    svg: ComponentFramework.PropertyTypes.StringProperty;
+    useFill: ComponentFramework.PropertyTypes.StringProperty;
+    fill: ComponentFramework.PropertyTypes.StringProperty;
+    fillSelected: ComponentFramework.PropertyTypes.StringProperty;
+    svgWidth: ComponentFramework.PropertyTypes.WholeNumberProperty;
+    svgHeight: ComponentFramework.PropertyTypes.WholeNumberProperty;
+    selectedID: ComponentFramework.PropertyTypes.StringProperty;
+    calloutID: ComponentFramework.PropertyTypes.StringProperty;
+}
+export interface IOutputs {
+    selectedID?: string;
+    calloutID?: string;
+}
